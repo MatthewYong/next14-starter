6b83fe7ae2b15637d1863f9663577a6df855508e
Matthew <matthew.yong@elementalconcept.com>
2024-03-18 21:18:06 +0000

    Add pages and navbar

---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,9 @@
+import styles from './navLink.module.css'
+
+const NavLink = ({item}) => {
+    return (
+        <div className={styles.container}>NavLink</div>
+    );
+};
+
+export default NavLink;
