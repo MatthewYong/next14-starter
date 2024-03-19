7281c42a6d8b6d05aab3312713943e0108dc1d56
Matthew <matthew.yong@elementalconcept.com>
2024-03-19 21:40:03 +0000

    Add external images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css"
+
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div>
+            <div className={styles.imgContainer}>
+                {/*<Image src="/about.jpg" alt="" fill/>*/}
+                <Image src="https://images.pexels.com/photos/20608917/pexels-photo-20608917/free-photo-of-a-motorcycle-parked-in-front-of-a-building-with-a-flower-garden.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/>
+            </div>
+        </div>
     );
 };
 
