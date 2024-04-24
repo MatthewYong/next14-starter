a158ed78dfc74bf6b61a5751d2224937f432bd76
Matthew <matthew.yong@elementalconcept.com>
2024-04-24 20:38:02 +0100

    Add single blog post

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,41 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css"
+
 const SinglePostPage = () => {
     return (
-        <div>SinglePostPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/20888059/pexels-photo-20888059/free-photo-of-view-of-a-canal-and-basilica-in-venice.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    fill
+                    className={styles.img}
+                />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image
+                        className={styles.avatar}
+                        src="https://images.pexels.com/photos/20888059/pexels-photo-20888059/free-photo-of-view-of-a-canal-and-basilica-in-venice.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt=""
+                        width={50}
+                        height={50}
+                    />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>Matthew Yong</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    Text Text Text TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText Text
+                </div>
+            </div>
+        </div>
     );
 };
 
